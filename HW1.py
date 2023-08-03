aefe3cb7b0489a5e6bd2a296eb6f99dd99df927d
PaulLysenko <p0101069904@gmail.com>
2023-08-03 14:36:26 +0300

    commit1

---
+++ HW1.py
@@ -0,0 +1,31 @@
+#1.Задача: Створіть дві змінні first=10, second=30. Виведіть на екран результат математичної взаємодії (+, -, *, / и тд.) для цих чисел.
+first = 10
+second = 30
+result_1 = first + second
+print(result_1)
+result_2 = first - second
+print(result_2)
+result_3 = first * second
+print(result_3)
+result_4 = first / second
+print(result_4)
+result_5 = first // second
+print(result_5)
+
+#Задача: Створіть змінну і почергово запишіть в неї результат порівняння (<, > , ==, !=) чисел з завдання 1. Виведіть на екран результат кожного порівняння.
+my_bool1 = True
+my_bool2 = False
+bool_resalt1 = first < second
+print(bool_resalt1)
+bool_resalt2 = first > second
+print(bool_resalt2)
+bool_resalt3 = first == second
+print(bool_resalt3)
+bool_resalt4 = first != second
+print(bool_resalt4)
+
+#Задача: Створіть змінну - результат конкатенації (складання) строк str1="Hello " и str2="world". Виведіть на екран.
+str1 = 'Hello'
+str2 = 'world'
+str_result = str1 + str2
+print(str_result)
