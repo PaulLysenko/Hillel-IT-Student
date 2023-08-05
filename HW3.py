a1366d1333d05c65510abacd77d3dd2e7fe10495
PaulLysenko <p0101069904@gmail.com>
2023-08-05 21:20:01 +0300

    commit3

---
+++ HW3.py
@@ -0,0 +1,16 @@
+while True:
+     user_input = input('Введіть Ваш вік : ')
+     if user_input.isdigit():
+         user_input = int(user_input)
+     elif user_input < age1
+         print('Де твої батьки?')
+    elif user_input < age2
+         print('Цей фільм для дорослих')
+    elif user_input > age3
+         print('Покажіть пенсійне посвідчення')
+     if user_input.__contains__(7)
+         print('Вам пощастить!')
+         print('А білетів вже немає!')
+         break
+     else:
+        print('Неправильно! Введіть ЧИСЛО.')
